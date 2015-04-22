af44e8ee22457842125cb387a0d7ef4c89bbfcb4
Elliot Reed <elliotreed2@gmail.com>
2015-04-21 21:30:56 -0700

    end of day, adding require.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+require(['../scripts'], function (common) {
+
+  var testAsteroid = new Asteroids.Asteroid([150, 150]);
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var testView = new Asteroids.GameView(context);
+  testView.start();
+})
